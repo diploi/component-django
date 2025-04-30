4ee8738e6e7f39218f25d284959b1103ca8c6696
wickathou <javier@enddesign.co>
2025-04-30 21:47:07 +0300

    Fixed missing static_root

--- djangoapp/settings.py
+++ djangoapp/settings.py
@@ -129,7 +129,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-<<<<<<< HEAD:djangoapp/settings.py
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
@@ -137,6 +136,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-=======
-STATIC_URL = '/static/'
->>>>>>> main:example/settings.py
